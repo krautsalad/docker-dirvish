717fc12ead87da408ce3d2b4407ff5b7baed4a9e
Sebastian Helbig <sebastian.helbig@krautsalad.com>
2025-08-31 13:02:12 +0200

    restructured build

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -ex
 
-docker build --no-cache --progress=plain -t krautsalad/dirvish:latest -f docker/Dockerfile .
+DIRVISH_VERSION=1.2.1
+
+docker build \
+    --build-arg DIRVISH_VERSION=${DIRVISH_VERSION} \
+    --no-cache --progress=plain -t krautsalad/dirvish:latest -f docker/Dockerfile .
 docker push krautsalad/dirvish:latest
 
 VERSION=$(git describe --tags "$(git rev-list --tags --max-count=1)")
